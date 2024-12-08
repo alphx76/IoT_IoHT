97ecf8d883edce39f4097b19f15a187a5aa0b99b
Dr. José Alfonso D. C. <95945447+alphx76@users.noreply.github.com>
2024-12-08 13:32:26 -0600

    Update Subscribe_sala_enefermeria.py

--- Subscribe_sala_enefermeria.py
+++ Subscribe_sala_enefermeria.py
@@ -1,3 +1,7 @@
+# JOSE ALFONSO DOMINGUEZ CHAVEZ 1/DIC2024
+# EJEMPLO DE USO MQTT Y JSON PARA UNA APLICACIÓN IOHT
+# CÓDIGO PARA LOS SIBSCRIPTORES
+
 import paho.mqtt.client as mqtt  # Importa la librería para el cliente MQTT
 import time                      # Importa la librería para el manejo del tiempo
 
