4754fc14c061db30a61257840c3f04c26136c4b4
Dr. José Alfonso D. C. <95945447+alphx76@users.noreply.github.com>
2025-03-03 11:18:26 -0600

    Update Publish_sala_enfermeria.py

--- Publish_sala_enfermeria.py
+++ Publish_sala_enfermeria.py
@@ -1,6 +1,7 @@
 # JOSE ALFONSO DOMINGUEZ CHAVEZ 1/DIC2024
 # EJEMPLO DE USO MQTT Y JSON PARA UNA APLICACIÓN IOHT
 # CÓDIGO PARA PUBLICAR
+# alfdominguez@uv.mx
 
 import paho.mqtt.client as mqtt  # Importa la librería para el cliente MQTT
 import json                      # Importa la librería para manejar JSON
